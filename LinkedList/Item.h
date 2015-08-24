5e44825a9883a2b5cc0d3f3604240df622327174
esquireatlarge <mattsguerri@gmail.com>
2015-08-24 16:43:42 -0700

    Initial

---
+++ LinkedList/Item.h
@@ -0,0 +1,21 @@
+//Esquire At Large
+//Represents an item storable within a linked list.
+
+template <class T>
+class Item
+{
+public:
+
+    Item(T data)
+        : m_data(data), m_next(NULL)
+    {
+    }
+
+    inline T GetData(){ return m_data };
+    inline Item<T>* GetNext(){ return m_next; }
+    void SetNext(Item<T>* n){ m_next = n; }
+
+private:
+    T        m_data;
+    Item<T>* m_next;
+};
